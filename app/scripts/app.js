adc3bd5b133dcbf756e3d6611d4cbf5d4d210e54
Michael Opper Moser <waiting4tzeentch@gmail.com>
2016-02-24 12:39:50 -0500

    Ran Yeoman and cleaned up index.html

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cis411Homework6App
+ * @description
+ * # cis411Homework6App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cis411Homework6App', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cis411Homework6App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cis411Homework6App
+ */
+angular.module('cis411Homework6App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
