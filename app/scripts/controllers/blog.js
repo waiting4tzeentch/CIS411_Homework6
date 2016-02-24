7537b61272580b20664adfc68713ce6039db9fba
Michael Opper Moser <waiting4tzeentch@gmail.com>
2016-02-24 12:59:46 -0500

    created blog html, blog js

---
+++ app/scripts/controllers/blog.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('cis411Homework6App')
+  .controller('BlogCtrl', function () {
+    this.nodeJs = [
+      'About Node.js',
+      'Node is difference',
+      'I/O Stuff'
+    ];
+  });
